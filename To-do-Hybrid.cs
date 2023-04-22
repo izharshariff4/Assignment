fa8660c6c5bb84cb8f0eaa3fc324e52d43da1746
Izhar Shariff <114794280+izharshariff4@users.noreply.github.com>
2023-04-22 19:23:47 +0530

    Add files via upload

---
+++ To-do-Hybrid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    enum Hybrid
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday
+    }
+    class To_do_Hybrid
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working Days List are");
+            Array weeks = Enum.GetValues(typeof(Hybrid));
+            for (int i = 0; i < weeks.Length; i++)
+            {
+                Console.WriteLine(weeks.GetValue(i));
+            }
+
+            Hybrid select = (Hybrid)Enum.Parse(typeof(Hybrid), Console.ReadLine(), true);
+            Console.WriteLine("the Selected day is " + select);
+        }
+    }
+}
